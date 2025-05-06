fedbce2234730b2316948de0d3fabae36303242d
K-LAGUAW <florestous@gmail.com>
2025-05-06 12:12:09 -0300

    first commit

---
+++ apps/api/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Shipments(models.Model):
+    id = models.AutoField(primary_key=True)
---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('apps.api.urls')),  # Endpoints de la API
+    path('', include('apps.web.urls')),  # Endpoints del frontend
+]
